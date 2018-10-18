e98a5239b2617aaa9804d141a8f5b349ab23d7c5
SARATOV\Daniil_Vadeneev <daniil_vadeneev@epam.com>
2018-10-18 09:40:52 +0400

    added babel polyfill for ie11

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: {
-        'index': path.resolve(__dirname, './src/index.js')
+        'index': ["@babel/polyfill", path.resolve(__dirname, './src/index.js')]
     },
 
     output: {
